74cd7478e81e5710719e524d62b84bac8082d139
Brad Geesaman <3769609+bgeesaman@users.noreply.github.com>
2023-04-12 08:17:13 -0400

    sbom demo

--- demo/sbom.sh
+++ demo/sbom.sh
@@ -9,26 +9,35 @@ clear
 
 echo ""
 
-p "# Generate SBOMs for 0-base and 5-zero images"
+p "# Generate SBOMs for 0-base and 6-sbom images"
 echo "Trivy 0-base generate sbom"
 sleep 1
-echo "Trivy 5-zero generate sbom"
+echo "Trivy 6-sbom generate sbom"
 sleep 0.2
 echo "Syft 0-base generate sbom"
 sleep 1
-echo "Syft 5-zero generate sbom"
+echo "Syft 6-sbom generate sbom"
 sleep 0.2
 
-p "# Scan the 0-base and 5-zero SBOMs with Trivy"
+p "# Scan the 0-base and 6-sbom SBOMs with Trivy"
 make trivy-0-base-sbom-summary
-make trivy-5-zero-sbom-summary
+make trivy-6-sbom-sbom-summary
 
-p "# Scan the 0-base and 5-zero SBOMs with Grype"
+p "# Scan the 0-base and 6-sbom SBOMs with Grype"
 make grype-0-base-sbom-summary
-make grype-5-zero-sbom-summary
+make grype-6-sbom-sbom-summary
 
-p "# View results"
-clear
-# make results-5-zero-sbom
+p "# List our SBOM results"
+ls -al results/sbom-*
 
-pe "clear"
+p "# View a Syft SBOM for 0-base"
+jless results/sbom-syft-0-base.json
+p "# View a Trivy SBOM for 0-base"
+jless results/sbom-trivy-scan-0-base.json
+p "# View a Trivy SBOM for 6-sbom"
+jless results/sbom-trivy-scan-6-sbom.json
+
+p "# Is \"eicar\" anywhere in the results?"
+pe "grep eicar results/*"
+
+p ""
