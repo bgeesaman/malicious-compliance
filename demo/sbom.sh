2cd4664416ee74d1de8c219b0a202390ee61d42f
Brad Geesaman <3769609+bgeesaman@users.noreply.github.com>
2023-04-13 07:49:40 -0400

    sbom demo

--- demo/sbom.sh
+++ demo/sbom.sh
@@ -19,20 +19,6 @@ sleep 1
 echo "Syft 5-zero generate sbom"
 sleep 0.2
 
-p "# Show Trivy's original image scan results of 0-base"
-make trivy-results-0-base-summary
-p "# Now, scan the 0-base and 5-zero image SBOMs with Trivy"
-make trivy-0-base-sbom-summary
-make trivy-5-zero-sbom-summary
-
-pe "clear"
-
-p "# Show Grype's original image scan results of 0-base"
-make grype-results-0-base-summary
-p "# Now, scan the 0-base and 5-zero image SBOMs with Grype"
-make grype-0-base-sbom-summary
-make grype-5-zero-sbom-summary
-
 p "# List our SBOM results"
 ls -al results/sbom-*
 
@@ -50,10 +36,29 @@ docker run -it --rm sig-honk/malicious-compliance:0-base ls -al /bin/bash
 
 p "# Use docker run to show /bin/bash in 5-zero"
 docker run -it --rm sig-honk/malicious-compliance:5-zero ls -al /bin/bash
-p "# Contents of /bin/bash in 5-zero:"
+p "# Show the contents of /bin/bash in 5-zero"
 docker run -it --rm sig-honk/malicious-compliance:5-zero cat /bin/bash
 echo ""
 
 p "# Is \"eicar\" anywhere in the results?"
 pe "grep -i eicar results/*"
 echo ""
+
+pe "clear"
+
+p "# Show Trivy's original image scan results of 0-base"
+make trivy-results-0-base-summary
+p "# Now, scan the 0-base and 5-zero image SBOMs with Trivy"
+make trivy-0-base-sbom-summary
+make trivy-5-zero-sbom-summary
+
+pe "clear"
+
+p "# Show Grype's original image scan results of 0-base"
+make grype-results-0-base-summary
+p "# Now, scan the 0-base and 5-zero image SBOMs with Grype"
+make grype-0-base-sbom-summary
+make grype-5-zero-sbom-summary
+
+echo ""
+p "# end"
